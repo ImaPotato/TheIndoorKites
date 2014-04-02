fbc3d82c72ed1de45b4846fa71eda7d8baa665d6
ImaPotato <Scott.a@outlook.com>
2014-04-02 14:27:21 +1300

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TheIndoorKites::Application.config.secret_key_base = 'a75c55a33d48eadec13d3dd855a0fdd0b3ef02c18544226721e05f228aa98aad9e851bb42cf2cb98db13b10174a751befe947b1bc5c558a284e95dba122fa7bb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TheIndoorKites::Application.config.session_store :cookie_store, key: '_TheIndoorKites_session'
