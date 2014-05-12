76f70af5840bea9b8d52d4213c34596a69845505
ImaPotato <Scott.a@outlook.com>
2014-05-12 12:25:32 +1200

    add/edit company page

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,14 @@ TheIndoorKites::Application.routes.draw do
   get "log_in" => "sessions#new", :as => "log_in"
   get "log_out" => "sessions#destroy", :as => "log_out"
   get "sign_up" => "users#new", :as => "sign_up"
-  get "homepage" => "sessions#redirect_to_homepage", :as => "homepage"
+  get "homepage" => "company#new", :as => "homepage"
 
 
   root :to => "users#new"
+
   resources :users
   resources :sessions
-
+  resources :companies
 
 
   # The priority is based upon order of creation: first created -> highest priority.
--- db/migrate/20140508032202_create_companies.rb
+++ db/migrate/20140508032202_create_companies.rb
@@ -1,6 +1,7 @@
 class CreateCompanies < ActiveRecord::Migration
   def change
     create_table :companies do |t|
+      t.string :company_name
       t.string :to
       t.string :from
       t.string :weight
