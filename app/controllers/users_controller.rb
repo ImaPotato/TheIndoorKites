e9256a7e231bb4cde9a1efa0a517cbeb67f04ecb
Max Godfrey <maxdgodfrey@gmail.com>
2014-05-20 22:46:22 +1200

    Finished adding 'manager' user (it's just a flag on the user model) - It's rather yucky I know, but hey be gentle - rails isn't :( - so now there is a new_user variable that is the user being created - and the old user variable is the currently logged in user.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,20 @@
 class UsersController < ApplicationController
   def new
-    @user = User.new
+      @user = current_user
+      @new_user = User.new
   end
 
   def create
-    @user = User.new(user_params)
-    if @user.save
+    @new_user = User.new(user_params)
+
+    if params[:commit] == 'Create Manager'
+        # set the is manager to true
+        @new_user.is_manager = true
+    else
+        @new_user.is_manager = false
+    end
+    
+    if @new_user.save
       redirect_to root_url, :notice => 'Signed up'
     else
       render 'new'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+manager = User.create({ email: "admin@kps.com", password:"password"  ,is_manager: true})
