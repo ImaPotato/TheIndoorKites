b9bae6637e6d6ae16f809e2a32ea5d6754baa050
Benjamin Riddell <bsriddell@Benjamins-MacBook-Air.local>
2014-05-21 14:01:58 +1200

    Updated controller and initial work for log view

--- app/controllers/business_controller.rb
+++ app/controllers/business_controller.rb
@@ -3,8 +3,14 @@ class BusinessController < ApplicationController
 
 	# GET /business
   def index
-    is_logged_in
+    #is_logged_in
+    all_events = History.all.compact
+    @events = all_events.sort_by { |a,b| a.time <=> b.time }.take(20)
+
+    #ruby rails generate model
     # todo will need to get all the relevant models
   end
+
+
   
 end
