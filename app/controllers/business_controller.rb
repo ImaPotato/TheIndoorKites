b74f780d16363fcd46e0c404fca098a38aa23e1f
Max Godfrey <maxdgodfrey@gmail.com>
2014-05-22 14:44:47 +1200

    Added displaying of the history

--- app/controllers/business_controller.rb
+++ app/controllers/business_controller.rb
@@ -4,8 +4,7 @@ class BusinessController < ApplicationController
 	# GET /business
   def index
     #is_logged_in
-    all_events = History.all.compact
-    @events = all_events.sort_by { |a,b| a.time <=> b.time }.take(20)
+   	@events = History.order("created_at DESC").limit(10)
 
     #ruby rails generate model
     # todo will need to get all the relevant models
